68333313a5fb100651d5a0969b328b59f0888e9b
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-11-30 16:25:36 +0300

    init commit

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,43 +1,39 @@
 import React, { ReactNode } from 'react';
 import Link, { LinkProps } from 'next/link';
-import classNames from 'classnames';
-import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
 interface LinkComponentProps extends Omit<LinkProps, 'href'> {
   children: ReactNode;
-  skipLocaleHandling?: boolean;
   href?: string;
-  className?: string; // Additional custom classes
-  onClick?: () => void; // Optional onClick handler
+  skipLocaleHandling?: boolean;
+  locale?: string;
 }
 
 const LinkComponent: React.FC<LinkComponentProps> = ({
   children,
   skipLocaleHandling = false,
-  href = '',
-  className,
-  onClick, // Extract onClick
+  href,
+  locale,
   ...rest
 }) => {
-  const {
-    i18n: { language: locale },
-  } = useTranslation();
+  const router = useRouter();
+  const currentLocale = locale || (router.query.locale as string) || '';
 
-  let computedHref = href;
+  let resolvedHref = href || router.asPath;
 
-  if (computedHref.startsWith('http')) {
+  if (resolvedHref.startsWith('http')) {
     skipLocaleHandling = true;
   }
 
-  if (locale && !skipLocaleHandling) {
-    computedHref = `/${locale}${computedHref}`;
+  if (currentLocale && !skipLocaleHandling) {
+    resolvedHref = resolvedHref
+      ? `/${currentLocale}${resolvedHref}`
+      : router.pathname.replace('[locale]', currentLocale);
   }
 
   return (
-    <Link href={computedHref} legacyBehavior passHref {...rest}>
-      <a className={classNames(className)} onClick={onClick}>
-        {children}
-      </a>
+    <Link href={resolvedHref} {...rest} legacyBehavior>
+      <a>{children}</a>
     </Link>
   );
 };
