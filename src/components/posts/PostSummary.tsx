4ba542d564f08a8c02ec0ddf3b8e2d13b28f442a
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-12-02 19:14:41 +0300

    init commit

--- src/components/posts/PostSummary.tsx
+++ src/components/posts/PostSummary.tsx
@@ -12,22 +12,21 @@ interface PostCardProps {
 
 export default function PostSummary({ post }: PostCardProps) {
   const { id, title, date, summary, thumbnail, topics } = post;
-  const { t } = useTranslation('post'); // Localization için ekleme
+  const { t } = useTranslation('post');
 
   return (
     <div className="post-card d-flex align-items-center mb-4">
       <div className="post-card-content flex-grow-1">
-        <h2 className="fw-bold mb-2">
+        <h2 className="fw-bold mb-4">
           <Link href={`/posts/${id}`} className="link">
             {title}
           </Link>
         </h2>
-        <p className="text-muted mb-2">{summary}</p>
         <p className="text-muted">
           <DateDisplay date={date} />
         </p>
         {topics && topics.length > 0 && (
-          <div className="mt-2">
+          <div className="mb-4">
             {topics.map(topic => (
               <Badge key={topic} bg="secondary" className="me-2">
                 {topic}
@@ -35,7 +34,8 @@ export default function PostSummary({ post }: PostCardProps) {
             ))}
           </div>
         )}
-        <div className="mt-3">
+        <p className="text-muted mb-4">{summary}</p>
+        <div className="mt-4">
           <Link href={`/posts/${id}`}>
             <Button className="primary">{t('post.readMore')}</Button>
           </Link>
