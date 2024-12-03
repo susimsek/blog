d1147fb144ce148b0554eb38306028b464cc5bd7
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-12-03 21:06:28 +0300

    init commit

--- __tests__/components/common/Link.test.tsx
+++ __tests__/components/common/Link.test.tsx
@@ -175,4 +175,144 @@ describe('Link', () => {
     const link = screen.getByText('About Us').closest('a');
     expect(link).toHaveAttribute('href', '/fr-FR/about');
   });
+
+  it('uses the provided locale', () => {
+    const locale = 'fr-FR';
+    render(
+      <Link href="/about" locale={locale}>
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', '/fr-FR/about');
+  });
+
+  it('uses the locale from the router query if no locale prop is provided', () => {
+    // Mock the router to have a locale in query
+    (useRouter as jest.Mock).mockReturnValue({
+      query: { locale: 'es-ES' },
+      asPath: '/current-path',
+      pathname: '/current-path',
+    });
+
+    render(
+      <Link href="/about">
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', '/es-ES/about');
+  });
+
+  it('falls back to the default locale if both locale and router.query.locale are undefined', () => {
+    // Mock the router with no locale in query
+    (useRouter as jest.Mock).mockReturnValue({
+      query: {},
+      asPath: '/current-path',
+      pathname: '/current-path',
+    });
+
+    render(
+      <Link href="/about">
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', '/about'); // This assumes default locale is not prefixed
+  });
+
+  it('prepends currentLocale to resolvedHref when locale is provided and skipLocaleHandling is false', () => {
+    const locale = 'fr-FR';
+    const resolvedHref = '/about';
+    (useRouter as jest.Mock).mockReturnValue({
+      query: { locale: 'en-US' },
+      asPath: '/current-path',
+      pathname: '/current-path',
+    });
+
+    render(
+      <Link href={resolvedHref} locale={locale}>
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', `/fr-FR${resolvedHref}`);
+  });
+
+  it('uses router.pathname when resolvedHref is not provided and locale is provided', () => {
+    const locale = 'fr-FR';
+    const resolvedHref = '';
+    (useRouter as jest.Mock).mockReturnValue({
+      query: { locale: 'en-US' },
+      asPath: '/current-path',
+      pathname: '/posts/[locale]/index',
+    });
+
+    render(
+      <Link href={resolvedHref} locale={locale}>
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', '/posts/fr-FR/index');
+  });
+
+  it('does not modify href when skipLocaleHandling is true', () => {
+    const resolvedHref = '/about';
+    (useRouter as jest.Mock).mockReturnValue({
+      query: { locale: 'en-US' },
+      asPath: '/current-path',
+      pathname: '/current-path',
+    });
+
+    render(
+      <Link href={resolvedHref} skipLocaleHandling>
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', resolvedHref);
+  });
+
+  it('does not modify href when currentLocale is undefined', () => {
+    const resolvedHref = '/about';
+    (useRouter as jest.Mock).mockReturnValue({
+      query: {},
+      asPath: '/current-path',
+      pathname: '/current-path',
+    });
+
+    render(
+      <Link href={resolvedHref}>
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', resolvedHref);
+  });
+
+  it('handles the case when resolvedHref is empty and locale is defined', () => {
+    const locale = 'fr-FR';
+    (useRouter as jest.Mock).mockReturnValue({
+      query: { locale: 'en-US' },
+      asPath: '/current-path',
+      pathname: '/posts/[locale]/index',
+    });
+
+    render(
+      <Link href="" locale={locale}>
+        <span>About Us</span>
+      </Link>,
+    );
+
+    const link = screen.getByText('About Us').closest('a');
+    expect(link).toHaveAttribute('href', '/posts/fr-FR/index');
+  });
 });
