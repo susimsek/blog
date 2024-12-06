3aee87303f490bfd035e916aaa99803ab167af93
susimsek <susimsek@users.noreply.github.com>
2024-12-06 17:01:46 +0000

    deploy: 2169ee4602e31e86af120b8083b75d0024277cf1

--- _next/static/7cYdOJprek7lxTB05tIjI/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002F404","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fposts\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
