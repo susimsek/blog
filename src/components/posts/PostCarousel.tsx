7ebc8d73cef1847a18b1c80116533f6854346de0
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-12-08 17:19:19 +0300

    init commit

--- src/components/posts/PostCarousel.tsx
+++ src/components/posts/PostCarousel.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Badge, Carousel } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight, faCircle } from '@fortawesome/free-solid-svg-icons';
 import { PostSummary } from '@/types/posts';
 import { assetPrefix } from '@/config/constants';
 import Link from '@/components/common/Link';
@@ -19,44 +19,65 @@ export default function PostCarousel({ posts, interval = 5000 }: Readonly<PostCa
   };
 
   return (
-    <Carousel
-      className="mx-auto shadow-sm rounded overflow-hidden"
-      style={{ maxWidth: '800px' }}
-      prevIcon={<FontAwesomeIcon className="carousel-control-prev-icon" icon="chevron-left" size="lg" />}
-      nextIcon={<FontAwesomeIcon className="carousel-control-next-icon" icon="chevron-right" size="lg" />}
-      activeIndex={activeIndex}
-      onSelect={handleSelect}
-      interval={interval}
-      wrap={true}
-    >
-      {posts.map(post => (
-        <Carousel.Item key={post.id}>
-          <Link href={`/posts/${post.id}`}>
-            <div className="thumbnail-wrapper">
-              <img src={`${assetPrefix}${post.thumbnail}`} alt={post.title} className="d-block w-100 rounded shadow" />
-            </div>
-          </Link>
-          <Carousel.Caption className="text-center bg-opacity-75 p-3 rounded" style={{ bottom: '2rem' }}>
-            <h3 className="fw-bold mb-3">
-              <Link href={`/posts/${post.id}`} className="link-light">
-                {post.title}
-              </Link>
-            </h3>
-            {post.topics && post.topics.length > 0 && (
-              <div className="mb-4">
-                {post.topics.map(topic => (
-                  <Link key={topic.id} href={`/topics/${topic.id}`}>
-                    <Badge bg={topic.color} className={`me-2 badge-${topic.color}`}>
-                      {topic.name}
-                    </Badge>
-                  </Link>
-                ))}
+    <div className="position-relative">
+      <Carousel
+        className="mx-auto shadow-sm rounded overflow-hidden"
+        style={{ maxWidth: '800px' }}
+        prevIcon={<FontAwesomeIcon className="carousel-control-prev-icon" icon={faChevronLeft} size="lg" />}
+        nextIcon={<FontAwesomeIcon className="carousel-control-next-icon" icon={faChevronRight} size="lg" />}
+        activeIndex={activeIndex}
+        onSelect={handleSelect}
+        interval={interval}
+        wrap={true}
+        indicators={false} // Default indicators are disabled
+      >
+        {posts.map((post, index) => (
+          <Carousel.Item key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <div className="thumbnail-wrapper">
+                <img
+                  src={`${assetPrefix}${post.thumbnail}`}
+                  alt={post.title}
+                  className="d-block w-100 rounded shadow"
+                />
               </div>
-            )}
-            <p className="text-light">{post.summary}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+            </Link>
+            <Carousel.Caption className="text-center bg-opacity-75 p-3 rounded" style={{ bottom: '2rem' }}>
+              <h3 className="fw-bold mb-3">
+                <Link href={`/posts/${post.id}`} className="link-light">
+                  {post.title}
+                </Link>
+              </h3>
+              {post.topics && post.topics.length > 0 && (
+                <div className="mb-4">
+                  {post.topics.map(topic => (
+                    <Link key={topic.id} href={`/topics/${topic.id}`}>
+                      <Badge bg={topic.color} className={`me-2 badge-${topic.color}`}>
+                        {topic.name}
+                      </Badge>
+                    </Link>
+                  ))}
+                </div>
+              )}
+              <p className="text-light">{post.summary}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+
+      <div className="carousel-indicators">
+        {posts.map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`${activeIndex === index ? 'active' : ''}`}
+            onClick={() => handleSelect(index)}
+            aria-label={`Go to slide ${index + 1}`}
+          >
+            <FontAwesomeIcon className="icon" icon="circle" size="sm" />
+          </button>
+        ))}
+      </div>
+    </div>
   );
 }
