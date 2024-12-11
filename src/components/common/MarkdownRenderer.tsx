4e4eacfbecd2bfc6233f48fd04bfbfcbfc5cb5c3
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-12-11 07:51:37 +0300

    init commit

--- src/components/common/MarkdownRenderer.tsx
+++ src/components/common/MarkdownRenderer.tsx
@@ -11,44 +11,29 @@ interface MarkdownRendererProps {
 const MarkdownRenderer: React.FC<Readonly<MarkdownRendererProps>> = ({ content }) => {
   const theme = useAppSelector(state => state.theme.theme);
 
+  const components = {
+    code: ({ className, children }: React.HTMLAttributes<HTMLElement>) => (
+      <CodeBlock className={className} theme={theme}>
+        {children}
+      </CodeBlock>
+    ),
+    table: ({ children }: React.HTMLAttributes<HTMLTableElement>) => (
+      <table className="table table-striped table-bordered">{children}</table>
+    ),
+    th: ({ children }: React.HTMLAttributes<HTMLTableCellElement>) => <th>{children}</th>,
+    td: ({ children }: React.HTMLAttributes<HTMLTableCellElement>) => <td>{children}</td>,
+    ul: ({ children }: React.HTMLAttributes<HTMLUListElement>) => <ul className="list-group">{children}</ul>,
+    ol: ({ children }: React.HTMLAttributes<HTMLOListElement>) => <ol className="list-group-numbered">{children}</ol>,
+    li: ({ children }: React.HTMLAttributes<HTMLLIElement>) => <li className="list-group-item">{children}</li>,
+    a: ({ href, children, ...rest }: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
+      <a href={href} target="_blank" rel="noopener noreferrer" className="text-decoration-underline" {...rest}>
+        {children}
+      </a>
+    ),
+  };
+
   return (
-    <ReactMarkdown
-      remarkPlugins={[remarkGfm]}
-      components={{
-        code({ className, children }) {
-          return (
-            <CodeBlock className={className} theme={theme}>
-              {children}
-            </CodeBlock>
-          );
-        },
-        table({ children }) {
-          return <table className="table table-striped table-bordered">{children}</table>;
-        },
-        th({ children }) {
-          return <th>{children}</th>;
-        },
-        td({ children }) {
-          return <td>{children}</td>;
-        },
-        ul({ children }) {
-          return <ul className="list-group">{children}</ul>;
-        },
-        ol({ children }) {
-          return <ol className="list-group-numbered">{children}</ol>;
-        },
-        li({ children }) {
-          return <li className="list-group-item">{children}</li>;
-        },
-        a({ href, children }) {
-          return (
-            <a href={href} target="_blank" rel="noopener noreferrer" className="text-decoration-underline">
-              {children}
-            </a>
-          );
-        },
-      }}
-    >
+    <ReactMarkdown remarkPlugins={[remarkGfm]} components={components}>
       {content}
     </ReactMarkdown>
   );
