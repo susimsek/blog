15d5887db421f9f5442e877f3b53fca5539a6f64
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-12-13 20:34:50 +0300

    init commit

--- __tests__/components/common/MarkdownTabsRenderer.test.tsx
+++ __tests__/components/common/MarkdownTabsRenderer.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MarkdownTabsRenderer from '@/components/common/MarkdownTabsRenderer';
-import { act } from 'react-dom/test-utils';
 
 jest.mock('react-markdown', () => {
   return ({ children }: { children: React.ReactNode }) => <div>{children}</div>;
