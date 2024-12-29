98ddd3eaa27b887f84380dc54040db55e6c98f36
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-12-29 10:17:41 +0300

    init commit

--- __tests__/components/common/Layout.test.tsx
+++ __tests__/components/common/Layout.test.tsx
@@ -12,6 +12,22 @@ jest.mock('@/components/common/Footer', () => ({
   default: () => <footer data-testid="footer">Footer</footer>,
 }));
 
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
 describe('Layout Component', () => {
   it('renders the Header component', () => {
     render(
