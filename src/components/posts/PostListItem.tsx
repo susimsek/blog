e84f69a5db3036cac1024fae77771bce9e125dc8
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-12-30 20:45:49 +0300

    init commit

--- src/components/posts/PostListItem.tsx
+++ src/components/posts/PostListItem.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import DateDisplay from '@/components/common/DateDisplay';
 import { assetPrefix } from '@/config/constants';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Link from '@/components/common/Link';
 
 interface PostListItemProps {
   post: PostSummary;
@@ -19,32 +18,26 @@ export default function PostListItem({ post }: Readonly<PostListItemProps>) {
       {/* Thumbnail */}
       <Col xs={5} md={4} className="post-thumbnail-container">
         {thumbnail && (
-          <Link href={`/posts/${id}`}>
-            <div className="post-thumbnail">
-              <Image
-                src={`${assetPrefix}${thumbnail}`}
-                alt={title}
-                className="img-fluid rounded"
-                width={800}
-                height={600}
-              />
-            </div>
-          </Link>
+          <div className="post-thumbnail">
+            <Image
+              src={`${assetPrefix}${thumbnail}`}
+              alt={title}
+              className="img-fluid rounded"
+              width={800}
+              height={600}
+            />
+          </div>
         )}
       </Col>
       {/* Content */}
       <Col xs={7} md={8} className="d-flex flex-column justify-content-between">
         <div>
-          <h6 className="post-title">
-            <Link href={`/posts/${id}`} className="link">
-              {title}
-            </Link>
-          </h6>
+          <h6 className="post-title">{title}</h6>
           <p className="post-date text-muted mb-1">
-            <Link href={`/posts/${id}`} className="link link-muted d-block mb-1">
+            <span className="text-muted d-block mb-1">
               <FontAwesomeIcon icon="calendar-alt" className="me-2" />
               <DateDisplay date={date} />
-            </Link>
+            </span>
             <span className="text-muted d-block">
               <FontAwesomeIcon icon="clock" className="me-2" />
               {readingTime}
@@ -53,11 +46,9 @@ export default function PostListItem({ post }: Readonly<PostListItemProps>) {
           {topics && topics.length > 0 && (
             <div className="post-topics">
               {topics.map(topic => (
-                <Link key={topic.id} href={`/topics/${topic.id}`}>
-                  <Badge bg={topic.color} className={`post-topic-badge badge-${topic.color}`}>
-                    {topic.name}
-                  </Badge>
-                </Link>
+                <Badge bg={topic.color} className={`post-topic-badge badge-${topic.color}`}>
+                  {topic.name}
+                </Badge>
               ))}
             </div>
           )}
