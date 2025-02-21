ca3b288eb41f9a135c9e3b7e10c888f27c4b0a76
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-02-21 20:05:33 +0300

    init commit

--- src/components/common/ContactInfo.tsx
+++ src/components/common/ContactInfo.tsx
@@ -10,28 +10,28 @@ const ContactInfo = () => {
   return (
     <ul className="list-unstyled fs-5 mt-3">
       <li className="mb-3">
-        <FontAwesomeIcon icon="envelope" className="me-2 text-primary" />
+        <FontAwesomeIcon icon="envelope" className="me-2 email-logo" />
         <strong>{t('common.contactInfo.email')}:</strong>{' '}
         <a href={CONTACT_LINKS.email} className="text-decoration-none">
           {CONTACT_LINKS.email.replace('mailto:', '')}
         </a>
       </li>
       <li className="mb-3">
-        <FontAwesomeIcon icon={['fab', 'linkedin']} className="me-2 text-info" />
+        <FontAwesomeIcon icon={['fab', 'linkedin']} className="me-2 linkedin-brand-logo" />
         <strong>{SOCIAL_MEDIA_NAMES.linkedin}:</strong>{' '}
         <a href={CONTACT_LINKS.linkedin} target="_blank" rel="noopener noreferrer" className="text-decoration-none">
           {CONTACT_LINKS.linkedin}
         </a>
       </li>
       <li className="mb-3">
-        <FontAwesomeIcon icon={['fab', 'medium']} className="me-2 text-dark" />
+        <FontAwesomeIcon icon={['fab', 'medium']} className="me-2 medium-brand-logo" />
         <strong>{SOCIAL_MEDIA_NAMES.medium}:</strong>{' '}
         <a href={CONTACT_LINKS.medium} target="_blank" rel="noopener noreferrer" className="text-decoration-none">
           {CONTACT_LINKS.medium}
         </a>
       </li>
       <li className="mb-3">
-        <FontAwesomeIcon icon={['fab', 'github']} className="me-2 text-secondary" />
+        <FontAwesomeIcon icon={['fab', 'github']} className="me-2 github-brand-logo" />
         <strong>{SOCIAL_MEDIA_NAMES.github}:</strong>{' '}
         <a href={CONTACT_LINKS.github} target="_blank" rel="noopener noreferrer" className="text-decoration-none">
           {CONTACT_LINKS.github}
