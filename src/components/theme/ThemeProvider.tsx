a397c687ee2fc3fc654b371c7488068ade1dfd44
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-03-03 17:38:29 +0300

    init commit

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -1,23 +1,16 @@
-import { ReactNode, useEffect, useState } from 'react';
-import Loading from '@/components/common/Loading';
+import { ReactNode, useEffect } from 'react';
 import { useAppSelector } from '@/config/store';
 
 type ThemeProviderProps = {
   children: ReactNode;
 };
 
-export default function ThemeProvider({ children }: Readonly<ThemeProviderProps>) {
+export default function ThemeProvider({ children }: ThemeProviderProps) {
   const theme = useAppSelector(state => state.theme.theme);
-  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    setIsClient(true);
     document.body.className = theme;
   }, [theme]);
-
-  if (!isClient) {
-    return <Loading />;
-  }
 
   return <>{children}</>;
 }
