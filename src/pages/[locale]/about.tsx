f3037cb4cf77dad951518a17596676224b771e8f
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-03-05 22:58:22 +0300

    init commit

--- src/pages/[locale]/about.tsx
+++ src/pages/[locale]/about.tsx
@@ -3,12 +3,21 @@ import { Container, Card } from 'react-bootstrap';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
-import { AUTHOR_NAME, AVATAR_LINK, CONTACT_LINKS, EXPERIENCE_START_YEAR, SITE_URL } from '@/config/constants';
+import {
+  AUTHOR_NAME,
+  AVATAR_LINK,
+  CONTACT_LINKS,
+  EXPERIENCE_START_YEAR,
+  SITE_URL,
+  TWITTER_USERNAME,
+} from '@/config/constants';
 import { getStaticPaths } from '@/lib/getStatic';
 import Layout from '@/components/common/Layout';
 import ContactInfo from '@/components/common/ContactInfo';
 import { makePostProps } from '@/lib/posts';
 import { PostSummary, Topic } from '@/types/posts';
+import { useRouter } from 'next/router';
+import i18nextConfig from '../../../next-i18next.config';
 
 type AboutProps = {
   posts: PostSummary[];
@@ -18,13 +27,18 @@ type AboutProps = {
 export default function About({ posts, topics }: Readonly<AboutProps>) {
   const { t } = useTranslation(['about']);
 
+  const router = useRouter();
+
+  const currentLocale = (router.query.locale as string) || i18nextConfig.i18n.defaultLocale;
+
   const url = `${SITE_URL}/about`;
+  const localizedUrl = `${SITE_URL}/${currentLocale}/about`;
 
   const jsonLdData = {
     '@context': 'https://schema.org',
     '@type': 'Person',
     name: AUTHOR_NAME,
-    url: url,
+    url: localizedUrl,
     image: AVATAR_LINK,
     jobTitle: t('about.jobTitle'),
     email: CONTACT_LINKS.email,
@@ -36,9 +50,27 @@ export default function About({ posts, topics }: Readonly<AboutProps>) {
       <Head>
         <title>{t('about.meta.title')}</title>
         <meta name="description" content={t('about.meta.description')} />
+        <link rel="canonical" href={url} />
         <meta name="keywords" content={t('about.meta.keywords')} />
         <meta name="author" content={AUTHOR_NAME} />
-        <meta name="robots" content="index, follow" />
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1" />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content={t('about.meta.title')} />
+        <meta property="og:description" content={t('about.meta.description')} />
+        <meta property="og:type" content="profile" />
+        <meta property="og:url" content={localizedUrl} />
+        <meta property="og:site_name" content={t('common:common.siteName')} />
+        <meta property="og:image" content={AVATAR_LINK} />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:image:type" content="image/jpeg" />
+
+        {/* Twitter Card meta tags for sharing on Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={TWITTER_USERNAME} />
+        <meta name="twitter:site" content={TWITTER_USERNAME} />
+
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }} />
       </Head>
       <Container className="py-5" style={{ maxWidth: '800px' }}>
