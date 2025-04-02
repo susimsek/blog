6223a1059f7db36267a6f64a1fadc5d99759f3ee
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-04-02 20:02:01 +0300

    init commit

--- __tests__/components/common/Header.test.tsx
+++ __tests__/components/common/Header.test.tsx
@@ -35,6 +35,22 @@ jest.mock('@assets/images/logo.svg', () => ({
   ReactComponent: () => <svg data-testid="mock-logo" />,
 }));
 
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
 describe('Header', () => {
   beforeEach(() => {
     (useTranslation as jest.Mock).mockReturnValue({
