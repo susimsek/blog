7e04aa28bf12857e2750871deb2d37c509ba961c
susimsek <suaybsimsek58@gmail.com>
2026-02-04 07:47:37 +0000

    deploy: eaa8d45710725a73bb67918c6e93d5d49a43c47d

---
+++ _next/static/chunks/137c14618fa01871.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/b450f70d37485876.js",
+  "static/chunks/7e7527d08ce4660c.js",
+  "static/chunks/0ae1adedb0723c45.js",
+  "static/chunks/c340f359467360dc.js",
+  "static/chunks/e1dc559f591307d3.css",
+  "static/chunks/turbopack-670e17da302c64f7.js"
+])
